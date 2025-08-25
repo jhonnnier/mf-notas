182e7f701f619fc27d7700bca27d903f85546bf6
Andrés Torres <ctorres437@hotmail.com>
2025-08-24 20:04:48 -0500

    feat: añadido fetch

--- src/pages/Notas.tsx
+++ src/pages/Notas.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Notas(props) {
+  const [notas, setNotas] = useState("NA")
+
+    useEffect(() => {
+      fetch("http://localhost:5002/notas")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Respuesta de /notas:", data.notas);
+          setNotas(data.notas)
+        })
+        .catch((error) => {
+          console.error("Error en la petición /estudiantes:", error);
+        });
+    }, []);
   return (
-      <div>
-          <h3>Notas</h3>
-      </div>
+    <div>
+      <h3>{notas}</h3>
+    </div>
   );
 }
